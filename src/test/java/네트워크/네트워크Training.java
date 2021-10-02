0fde9d647ec4e58e054b5b4c90f9953f65d00579
= <=>
2021-10-02 10:35:35 +0900

    feat: add netwrok training

--- src/test/java/네트워크/네트워크Training.java
+++ src/test/java/네트워크/네트워크Training.java
@@ -10,22 +10,33 @@ public class 네트워크Training {
     @Test
     void test(){
         int answer = 0;
-        boolean[] visited = new boolean[n];
-
-        for (int i=0; i < n; i++){
+        boolean visited[] = new boolean[3];
+        for (int i=0; i<n; i++){
             if (!visited[i]){
                 dfs(computer, visited, i);
                 answer++;
             }
         }
-
+        System.out.println(answer);
     }
 
-    /*
-    git push test
-     */
-    void dfs(int[][] computer, boolean[] visited, int count) {
-        visited[count] = true;
+    void dfs(int[][] computer, boolean[] visited, int start) {
+        visited[start] = true;
+
+        /*
+        {{1, 1, 0}, {1, 1, 0}, {0, 0, 1}}
+        true, false, false // start = 0, i = 0
+        true, false, false // start = 0, i = 1
+        true, true, false // start = 1, i = 0
+        true, true, false // start = 1, i = 1
+        true, true, false // start = 1, i = 2
+        true, true, false // start = 0, i = 2
+         */
+        for (int i=0; i<computer.length; i++){
+            if (computer[start][i] == 1 && visited[i] == false){
+                dfs(computer, visited, i);
+            }
+        }
 
     }
 
